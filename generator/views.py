ade631c7ac7d24d268aea1acc5f147fe842457da
Derfel Cadarn <gehigbie@gmail.com>
2020-02-01 21:45:25 -0700

    Add new input options

--- generator/views.py
+++ generator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'generator/home.html')
+    return render(request, 'generator/home.html', {'options': range(5, 20)})
 
 def password(request):
     return render(request, 'generator/password.html')
