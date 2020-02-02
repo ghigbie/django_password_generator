160dd30abaf930c2122635a967c9225c3b4358e5
Derfel Cadarn <gehigbie@gmail.com>
2020-02-01 22:14:45 -0700

    Generation is working

--- generator/views.py
+++ generator/views.py
@@ -6,18 +6,18 @@ def home(request):
     return render(request, 'generator/home.html', {'options': range(5, 20)})
 
 def password(request):
-    characters = list('abcdefghijklmnopqrs')
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    characters = list(alphabet)
     length = int(request.GET.get('length', 12))
 
     if request.GET.get('uppercase'):
-        uppercase = bool(request.GET.get('uppercase'))
-
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
     if request.GET.get('numbers'):
-        numbers = bool(request.GET.get('numbers'))
+        characters.extend(list('0123456789'))
 
-    if request.GET.get()'special'):
-        special = bool(request.GET.get('special'))
-        
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*?'))
+
     thepassword = ''
     for x in range(length):
         thepassword += random.choice(characters)
