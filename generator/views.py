235ef599c893fea57ec96daf4d79c363bb1b3ec9
Derfel Cadarn <gehigbie@gmail.com>
2020-02-01 21:30:19 -0700

    Edit views

--- generator/views.py
+++ generator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'generator/home.html', {'password': 'ADAFDFAF'})
+    return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html', {'password': '123abc345bcd'})
+    return render(request, 'generator/password.html')
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -18,5 +18,5 @@ from generator import views
 
 urlpatterns = [
    path('', views.home),
-   path('password', views.password),
+   path('generatedpassword/', views.password, name="password"),
 ]
