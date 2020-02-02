d791ae547a66aee9415d54e25713678049ec1459
Derfel Cadarn <gehigbie@gmail.com>
2020-02-01 21:59:21 -0700

    Edit function call

--- generator/views.py
+++ generator/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def home(request):
     return render(request, 'generator/home.html', {'options': range(5, 20)})
 
 def password(request):
-    return render(request, 'generator/password.html')
+    characters = list('abcdefghijklmnopqrs')
+    length = 10
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+    return render(request, 'generator/password.html', {'password': thepassword})
